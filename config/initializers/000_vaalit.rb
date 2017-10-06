38a093b407a3a86e0f08f89d6dbd51287a493478
Jukka Karvonen <jukka.karvonen@ayy.fi>
2017-10-06 11:46:15 +0000

    AYY council size

--- config/initializers/000_vaalit.rb
+++ config/initializers/000_vaalit.rb
@@ -9,7 +9,7 @@ module Vaalit
 
   module Voting
     PROPORTIONAL_PRECISION = 5   # Decimals used in proportional numbers (eg. 87.12345 is 5 decimals)
-    ELECTED_CANDIDATE_COUNT = 60 # How many candidates are elected
+    ELECTED_CANDIDATE_COUNT = 45 # How many candidates are elected
   end
 
   module Results
